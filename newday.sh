71f67bd03ca6b80bd1022e9d3e9e7b23b112e38b
ebenpack <ebenpackwood@gmail.com>
2020-12-11 15:26:38 -0500

    update newday script

--- newday.sh
+++ newday.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-touch "input/Day0$1.txt"
-cp test/Days/Day01Spec.hs "test/Days/Day0$1Spec.hs"
-sed -i.bak -e "s/Day01/Day0$1/g" "test/Days/Day0$1Spec.hs"
-rm "test/Days/Day0$1Spec.hs.bak"
+touch "input/Day$1.txt"
+cp test/Days/Day01Spec.hs "test/Days/Day$1Spec.hs"
+sed -i.bak -e "s/Day01/Day$1/g" "test/Days/Day$1Spec.hs"
+rm "test/Days/Day$1Spec.hs.bak"
